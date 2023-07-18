a9a047f311b4a7f092340a2ae7f174334d22ed2e
h8rry <harrychenca@gmail.com>
2023-07-18 23:41:36 +0200

    Fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_191624) do
+ActiveRecord::Schema.define(version: 2023_07_17_125500) do
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "token"
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
